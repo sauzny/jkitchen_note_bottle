bb3380a18f663f07a33d41e39ab304a30c6666e5
xinxin <xinxin_949421@yahoo.com>
2019-02-18 17:13:23 +0800

    update 16

---
+++ xxleetcode/src/main/java/com/sauzny/xxleetcode/p011_020/Solution016.java
@@ -0,0 +1,46 @@
+package com.sauzny.xxleetcode.p011_020;
+
+import java.util.Arrays;
+
+/***************************************************************************
+ *
+ * @时间: 2019/2/18 - 15:54
+ *
+ * @描述: TODO
+ *
+ ***************************************************************************/
+public class Solution016 {
+
+    public int threeSumClosest(int[] nums, int target) {
+        // 排序
+        Arrays.sort(nums);
+        int closestNum = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.length - 2; i++) {
+            int l = i + 1, r = nums.length - 1;
+            while (l < r) {
+                //System.out.println(nums[l]);
+                int threeSum = nums[l] + nums[r] + nums[i];
+                if (Math.abs(threeSum - target) < Math.abs(closestNum - target)) {
+                    closestNum = threeSum;
+                }
+                if (threeSum > target) {
+                    while (r > l && nums[r] == nums[r-1]) r--;
+                    r--;
+                } else if (threeSum < target) {
+                    while (l < r && nums[l] == nums[l+1]) l++;
+                    l++;
+                } else {
+                    // 如果已经等于target的话, 肯定是最接近的
+                    return target;
+                }
+            }
+        }
+        return closestNum;
+    }
+
+    public static void main(String[] args) {
+        Solution016 solution016 = new Solution016();
+        int result = solution016.threeSumClosest(new int[]{-1, 0, 1, 1, 55}, 3);
+        System.out.println(result);
+    }
+}
